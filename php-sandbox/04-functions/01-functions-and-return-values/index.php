ea1017c5f1b3953b76c72c907e38d986796e2734
Bertrand D <b.duclot@gmail.com>
2023-12-16 12:20:03 +0100

    Exercices

---
+++ php-sandbox/04-functions/01-functions-and-return-values/index.php
@@ -0,0 +1,31 @@
+<?php
+
+/*function add ($a = 1, $b = 2){
+    return $a + $b;
+}
+
+echo add();*/
+
+
+/*function addAll(...$numbers){
+    $total = 0;
+
+    foreach ($numbers as $number){
+        $total += $number;
+    }
+
+    return $total;
+}
+
+echo addAll(3,5,7,5);*/
+
+
+/* Optional type declaration
+
+function getSum(int $a, int $b): int {
+    return $a + $b;
+}*/
+
+
+
+
